f889ba49b4d014c8763efd3253ad54a369032757
GerardoArdon <20235947@esen.edu.sv>
2025-03-02 12:25:05 -0600

    Realización Guia 4 a excepcion de los complementarios

---
+++ GuiasDeTrabajo2/practica4/src/components/BudgetForm.jsx
@@ -0,0 +1,47 @@
+import { useState, useContext } from "react";
+import { BudgetDispatchContext } from "../context/BudgetContext";
+
+export const BudgetForm = () => {
+  const [budget, setBudget] = useState("");
+  const numericBudget = parseFloat(budget);
+  const isInvalid = budget === "" || isNaN(numericBudget) || numericBudget <= 0; // si no es un número > 0 es inválido
+
+  const handleChange = (e) => {
+    setBudget(e.target.value);
+  };
+
+  const dispatch = useContext(BudgetDispatchContext);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "add-budget",
+      payload: { budget: numericBudget }
+    });
+  };
+
+  return (
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+          Definir presupuesto
+        </label>
+      </div>
+      <input
+        type="number"
+        id="budget"
+        name="budget"
+        placeholder="Define tu presupuesto"
+        className="w-full bg-white border border-gray-200 p-2"
+        value={budget}
+        onChange={handleChange}
+      />
+      <input
+        type="submit"
+        value="Definir presupuesto"
+        disabled={isInvalid}
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+      />
+    </form>
+  );
+};
